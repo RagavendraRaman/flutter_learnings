ccdd951059db499fdf6028b2b3927ba19c626ecb
RagavendraRaman <ramanrag@ix.lentra.ai>
2023-08-10 12:34:10 +0530

    initial web socket meggese

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,85 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-import 'package:dice_application/gradient_container.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:dice_application/receive_messege.dart';
+
+import 'next_screen.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Scaffold(
-        body: GradientContainer(
-            [Color.fromARGB(255, 31, 2, 68), Color.fromARGB(255, 46, 2, 75)])),
-  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  
+  FlutterLocalNotificationsPlugin flutterLocalNotifications =
+      FlutterLocalNotificationsPlugin();
+  String message = '';
+
+  @override
+  void initState() {
+    super.initState();
+    getMessege(context);
+    _initLocalNotifications();
+    // _steamListner();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  void _initLocalNotifications() {
+    const AndroidInitializationSettings androidSettings =
+        AndroidInitializationSettings('mipmap/ic_launcher');
+    final InitializationSettings initializationSettings =
+        InitializationSettings(android: androidSettings);
+    flutterLocalNotifications.initialize(initializationSettings);
+  }
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('WebSocket Notifications 1'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const Text('Listening to WebSocket notifications...'),
+            ElevatedButton(
+              onPressed: (() {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                      builder: (BuildContext context) => MyWidget()),
+                );
+              }),
+              child: const Text('Next page'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
